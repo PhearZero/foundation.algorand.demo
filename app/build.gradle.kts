2cc8c35c8935465c21c3ecc43c537497adf9d443
Michael Feher <michael.feher@algorand.foundation>
2023-09-25 14:34:55 -0500

    chore: add simple webauthn

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -9,14 +11,30 @@ android {
 
     defaultConfig {
         applicationId = "foundation.algorand.demo"
-        minSdk = 28
+        minSdk = 21
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables.useSupportLibrary = true
+        buildConfigField("String", "API_BASE_URL", "\"https://chatter-remarkable-trout.glitch.me/auth\"")
+        resValue("string", "host", "https://chatter-remarkable-trout.glitch.me")
+        resValue(
+            "string", "asset_statements", """
+           [{
+             "include": "https://chatter-remarkable-trout.glitch.me/.well-known/assetlinks.json"
+           }]
+        """)
     }
-
+    signingConfigs {
+        getByName("debug") {
+            storeFile = file("../debug.jks")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
+    }
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -27,30 +45,76 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     buildFeatures {
-        viewBinding = true
+        dataBinding = true
+        buildConfig = true
     }
 }
-
+var kotlinVersion: String by rootProject.extra
 dependencies {
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-cbor:2.15.2")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
-    implementation("com.google.guava:guava:32.1.2-android")
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
+
+    implementation("com.google.android.gms:play-services-fido:20.0.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.fragment:fragment-ktx:1.6.0")
+
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
-    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
-    implementation("com.squareup.okhttp3:okhttp:4.11.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+
+    var coroutineVersion by extra { "1.7.1" }
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutineVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutineVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:$coroutineVersion")
+
+    var hiltVersion by extra { "2.47" }
+    implementation("com.google.dagger:hilt-android:$hiltVersion")
+    kapt("com.google.dagger:hilt-compiler:$hiltVersion")
+    kapt("androidx.hilt:hilt-compiler:1.0.0")
+
+    var lifecycleVersion by extra { "2.6.1" }
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
+
+    implementation("com.google.android.material:material:1.9.0")
+
+    var okhttpVersion by extra { "4.11.0" }
+    implementation("com.squareup.okhttp3:okhttp:$okhttpVersion")
+    implementation("com.squareup.okhttp3:logging-interceptor:$okhttpVersion")
+    implementation("ru.gildor.coroutines:kotlin-coroutines-okhttp:1.0")
+
+//    testImplementation 'junit:junit:4.13.2'
+//    androidTestImplementation 'androidx.test:runner:1.5.2'
+//    androidTestImplementation 'androidx.test:rules:1.5.0'
+//
+//    androidTestImplementation 'androidx.test.ext:junit:1.1.5'
+//    androidTestImplementation 'androidx.test.ext:truth:1.5.0'
+//    androidTestImplementation 'com.google.truth:truth:1.1.3'
+//
+//    androidTestImplementation 'androidx.test.espresso:espresso-core:3.5.1'
+
+//    val hiltVersion: String by extra {"2.48"}
+//    implementation("com.google.dagger:hilt-android:$hiltVersion")
+//    kapt("com.google.dagger:hilt-compiler:$hiltVersion")
+
+//    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+//    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-cbor:2.15.2")
+//    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
+//    implementation("com.google.guava:guava:32.1.2-android")
+//    implementation("androidx.core:core-ktx:1.9.0")
+//    implementation("androidx.appcompat:appcompat:1.6.1")
+//    implementation("com.google.android.material:material:1.9.0")
+//    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+//    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
+//    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+//    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+//    testImplementation("junit:junit:4.13.2")
+//    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+//    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- app/src/main/java/foundation/algorand/demo/MainActivity.kt
+++ app/src/main/java/foundation/algorand/demo/MainActivity.kt
@@ -1,27 +1,69 @@
 package foundation.algorand.demo
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import foundation.algorand.demo.databinding.ActivityMainBinding
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.lifecycle.lifecycleScope
+import com.google.android.gms.fido.Fido
+import dagger.hilt.android.AndroidEntryPoint
+//import foundation.algorand.demo.databinding.ActivityMainBinding
+import foundation.algorand.demo.fido2.repository.SignInState
+import foundation.algorand.demo.ui.auth.AuthFragment
+import foundation.algorand.demo.ui.home.HomeFragment
+import foundation.algorand.demo.ui.username.UsernameFragment
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
-
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        Toast.makeText(this@MainActivity, "loading", Toast.LENGTH_LONG).show()
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        lifecycleScope.launchWhenStarted {
+            Toast.makeText(this@MainActivity, "Launch on start", Toast.LENGTH_LONG).show()
+            viewModel.signInState.collect { state ->
+                when (state) {
+                    is SignInState.SignedOut -> {
+                        showFragment(UsernameFragment::class.java) { UsernameFragment() }
+                    }
+                    is SignInState.SigningIn -> {
+                        showFragment(AuthFragment::class.java) { AuthFragment() }
+                    }
+                    is SignInState.SignInError -> {
+                        Toast.makeText(this@MainActivity, state.error, Toast.LENGTH_LONG).show()
+                        // return to username prompt
+                        showFragment(UsernameFragment::class.java) { UsernameFragment() }
+                    }
+                    is SignInState.SignedIn -> {
+                        showFragment(HomeFragment::class.java) { HomeFragment() }
+                    }
+                }
+            }
+        }
+    }
+    override fun onResume() {
+        super.onResume()
+        viewModel.setFido2ApiClient(Fido.getFido2ApiClient(this@MainActivity))
+    }
 
-        setSupportActionBar(binding.toolbar)
+    override fun onPause() {
+        super.onPause()
+        viewModel.setFido2ApiClient(null)
+    }
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+    private fun showFragment(clazz: Class<out Fragment>, create: () -> Fragment) {
+        Toast.makeText(this@MainActivity, "show fragment", Toast.LENGTH_LONG).show()
+        val manager = supportFragmentManager
+        if (!clazz.isInstance(manager.findFragmentById(R.id.container))) {
+            manager.commit {
+                replace(R.id.container, create())
+            }
+        }
     }
 }
