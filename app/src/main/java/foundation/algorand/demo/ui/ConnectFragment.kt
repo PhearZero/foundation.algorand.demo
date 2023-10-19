9d9efc6c35d25ffeb2d1276cfe3bb66e180a1b55
Michael J Feher <github@phearzero.com>
2023-10-19 15:51:48 -0500

    chore: update activity baseURL

--- app/src/main/java/foundation/algorand/demo/ui/ConnectFragment.kt
+++ app/src/main/java/foundation/algorand/demo/ui/ConnectFragment.kt
@@ -1,7 +1,6 @@
 package foundation.algorand.demo.ui
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,9 +8,6 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
-import com.algorand.algosdk.builder.transaction.PaymentTransactionBuilder
-import com.algorand.algosdk.crypto.Address
-import com.algorand.algosdk.transaction.Transaction
 import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.codescanner.GmsBarcodeScanner
 import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
@@ -56,16 +52,13 @@ class ConnectFragment : Fragment() {
                 Toast.makeText(requireContext(), e.message, Toast.LENGTH_LONG).show()
             }
     }
-
-    // TODO: move to viewModel
     private fun parseBarcodeTransaction(barcode: Barcode) {
         val jObject = JSONObject(barcode.displayValue.toString())
-        // TODO: Parse transaction properties
         val origin = jObject.get("origin").toString()
         val requestId = jObject.get("requestId").toString().toDouble()
-        Log.d("COnnectFragment", requestId.toString())
         lifecycleScope.launch {
             viewModel.connectResponse(requestId)
+            (activity as MainActivity).updateBaseURL(origin)
         }
     }
 }
